6de8f44a4e3426369e9a858f960cdb29f7fc64f9
ezio84 <brabus84@gmail.com>
2018-06-22 17:28:16 +0200

    Add dynamic Immersive mode tile
    * On top of existing AOSP policy control code
    
    - Immersive statusbar
    - Immersive navbar
    - Full immersive
    - Tap to cycle modes
    - Long press to enable the selected immersive mode
    - If the current immersive mode is enabled, long pressing again the tile
      will disable immersive so no need to cycle the tile to go to the "off"
      position
    - Tile icon will be set to active or unactive state according to the
    current mode
    
    *DU Edits*
    - Switched to using switch case vs if/else statements (easier this way)
    - Changed QS tiles to match expanded desktop ones (credit mik9)
    https://github.com/DirtyUnicorns/android_packages_apps_DU-Tweaks/commit/60ffd67cf1f263fff5bcb1f572c8569b8bfe6486
    
    Change-Id: I1747b7f4077247014ca5e2a160712354df6e023e
    Signed-off-by: Josh Fox (XlxFoXxlX) <joshfox87@gmail.com>
    Signed-off-by: NotZeetaa <rodrigo2005contente@gmail.com>

--- packages/SystemUI/src/com/android/systemui/qs/tileimpl/QSFactoryImpl.java
+++ packages/SystemUI/src/com/android/systemui/qs/tileimpl/QSFactoryImpl.java
@@ -40,6 +40,7 @@ import com.android.systemui.qs.tiles.FlashlightTile;
 import com.android.systemui.qs.tiles.GamingModeTile;
 import com.android.systemui.qs.tiles.HeadsUpTile;
 import com.android.systemui.qs.tiles.HotspotTile;
+import com.android.systemui.qs.tiles.ImmersiveTile;
 import com.android.systemui.qs.tiles.LocationTile;
 import com.android.systemui.qs.tiles.NfcTile;
 import com.android.systemui.qs.tiles.NightDisplayTile;
@@ -214,6 +215,8 @@ public class QSFactoryImpl implements QSFactory {
                 return mHeadsUpTileProvider.get();
             case "gaming":
                 return mGamingModeTileProvider.get();
+            case "immersive":
+                return new ImmersiveTile(mHost);
         }
 
         // Custom tiles
